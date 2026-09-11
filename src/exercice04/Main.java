f2b6fe6e3e33bda1a70602e423fd9b2274fcfd06
julietterssgnl <juliette.rossignol18@gmail.com>
2026-09-11 11:09:25 +0200

    Exercice 4 fait mauvaise manip Main rempli

--- src/exercice04/Main.java
+++ src/exercice04/Main.java
@@ -1,3 +1,15 @@
-void main() {
-    
+package exercice04;
+
+public class Main {
+    public static void main(String[] args) {
+        Employe[] employes = new Employe[] {
+                new Employe("Bob", 2000),
+                new Manager("Alice", 3000, 800),
+                new Employe("Charlie", 2200)
+        };
+
+        for (Employe e : employes) {
+            System.out.println("Employé : " + e.getNom() + " | Salaire : " + e.calculerSalaire() + " €");
+        }
+    }
 }
